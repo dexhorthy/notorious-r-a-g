101e4483023ac9260c36a85c02a0844bb6178576
amantri <829820+amantri@users.noreply.github.com>
2024-10-12 11:48:16 -0700

    Remove extraneous chars

--- hacky-discord-stuff/discord_bot.py
+++ hacky-discord-stuff/discord_bot.py
@@ -4,8 +4,7 @@ from discord.ext import commands
 from llama_index.readers.discord import DiscordReader
 
 
-discord_token = os.getenv(":w
-                          DISCORD_BOT_TOKEN")
+discord_token = os.getenv("DISCORD_BOT_TOKEN")
 channel_ids = [1272969803921096779]  # Replace with your channel_id
 reader = DiscordReader(discord_token=discord_token)
 documents = reader.load_data(channel_ids=channel_ids)
