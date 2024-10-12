02fd19ede92fcad513298234da0133180de3f750
Roey Ben Chaim <roey.benhaim@gmail.com>
2024-10-12 15:50:21 -0700

    discord thread

--- api/discord_thread.py
+++ api/discord_thread.py
@@ -13,11 +13,19 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.channel.id not in [1294545886281469972]:
+    if message.author == client.user:
         return
 
-    if message.content.startswith("$hello"):
-        await message.channel.send("Hello!")
+    if (message.channel.id not in [1294545886281469972]) and message.channel.parent.id not in [1294545886281469972]:
+        return
+    if isinstance(message.channel, discord.Thread):
+        thread = message.channel
+        print(thread.name)
+        await thread.send(f"Responding in the thread: {thread.name}")
+    else:
+        # Create a new thread for the question
+        thread = await message.create_thread(name="New Question Thread", auto_archive_duration=60)
+        await thread.send("Hello! I've created a new thread for your question. How can I assist you?")
 
 
 async def launch_discord_listener(discord_token):
